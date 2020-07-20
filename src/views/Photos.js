68a2081bceb4c71b5e7731aa4aff04b7427d87b6
Brian Haack <haack79@hotmail.com>
2020-07-20 12:43:36 -0700

    updated image views

--- src/views/Photos.js
+++ src/views/Photos.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, {useContext} from "react";
+import Image from '../components/Image';
+import {getClass} from "../utils";
+import {ContextProvider} from "./Context"
+import { Context } from "../Context";
 
 function Photos() {
+    const {allPhotos} = useContext(ContextProvider);
+    const images = allPhotos.map((img, i) => (
+        <Image key={img.id} img={img} className={getClass(i)} />
+    ))
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            <h1>{images}</h1>
         </main>
     )
 }

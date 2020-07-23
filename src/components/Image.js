2e18fbbc87e7c85d8b176d24740c7fe3e7daf4b4
Brian Haack <haack79@hotmail.com>
2020-07-23 09:51:40 -0700

    updated ability to click and see favorited change

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,9 +4,17 @@ import {Context} from "../Context";
 
 const Image = ({className, img}) => {
     const [hovered, setHovered] = useState(false);
-    const {toggleFavorited} = useContext(Context); 
+    const {toggleFavorite} = useContext(Context); 
 
-    const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => {toggleFavorited(img.id)}}></i>
+    const heartIcon = () => {
+        if (img.isFavorited) {
+            return <i className="ri-heart-fill favorite" onClick={() => {toggleFavorite(img.id)}}></i>
+        } else if (hovered) { 
+            return <i className="ri-heart-line favorite" onClick={() => {toggleFavorite(img.id)}}></i>
+        }
+    }
+    // const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => {toggleFavorited(img.id)}}></i>
+
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
     return (
@@ -15,7 +23,7 @@ const Image = ({className, img}) => {
             onMouseLeave={() => setHovered(false)}
         >
             <img alt="animal or nature" src={img.url} className="image-grid"/>
-            {heartIcon}
+            {heartIcon()}
             {cartIcon}
         </div>
     )

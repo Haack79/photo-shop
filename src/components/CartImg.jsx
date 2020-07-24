18b9f43c1c5fac0e7807c5121758411aec59424a
Brian Haack <haack79@hotmail.com>
2020-07-24 11:53:42 -0700

    added ability to add img's to add to cart and carry to checkout

---
+++ src/components/CartImg.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CartImg = ({img}) => {
+    return (
+        <h2>{img.id}</h2>
+    )
+}
+export default CartImg; 
--- src/views/Cart.js
+++ src/views/Cart.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React, {useContext} from "react";
+import {Context} from "../Context";
+import CartImg from "../components/CartImg";
 
 function Cart() {
+    const {cartItems} = useContext(Context); 
+    const cartItemImages = cartItems.map(img => (
+        <CartImg key={img.id} img={img} />
+    ))
     return (
         <main className="cart-page">
             <h1>Check out</h1>
+            {cartItemImages}
         </main>
     )
 }
 
-export default Cart
+export default Cart;
